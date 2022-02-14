eecee9fa183eefc62380254dca8759a0898b350e
urirob <91110790+urirob@users.noreply.github.com>
2022-02-14 15:10:51 +0530

    Add files via upload

---
+++ assgn3_python_output.py
@@ -0,0 +1,79 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:\Users\DELL\OneDrive\Desktop\ques.py ===============
+Give a string-
+ASSIGNMENT
+{'A': 1, 'S': 2, 'I': 1, 'G': 1, 'N': 2, 'M': 1, 'E': 1, 'T': 1}
+enter month-
+2
+Give year-2020
+Give day-29
+Date- 29 / 2 / 2020
+Next Date- 1/ 3 / 2020
+Done!
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Give a number between 4 and 10 including them-
+9
+Performance=Excellent
+Letter Grade=A
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+----------------(a)-------------------
+Give the sid (a number): 
+20103105
+Enter your name: 
+TARUNDEEP
+give a letter y or n: 
+N
+The required dictionary-- {20103105: 'TARUNDEEP'}
+The list will be used in further parts--
+The required list-- [(20103105, 'TARUNDEEP')]
+----------------(b)------------------------
+The dictionary we had-
+{20103105: 'TARUNDEEP'}
+now we have exchanged the key value pair but its not sorted-
+{'TARUNDEEP': 20103105}
+The unsorted list--
+[('TARUNDEEP', 20103105)]
+The sorted list--
+[('TARUNDEEP', 20103105)]
+The sorted dictionary-
+{'TARUNDEEP': 20103105}
+This is the dictionary sorted on the basis of name-
+{20103105: 'TARUNDEEP'}
+-----------------(c)-----------------
+The list we made before will be used now-
+[(20103105, 'TARUNDEEP')]
+now the list is sorted-
+[(20103105, 'TARUNDEEP')]
+sorted dictionary based on sid- {20103105: 'TARUNDEEP'}
+-----------------(d)-----------------
+Please enter one of the sids you entered before-
+20103105
+The name of the student with the entered sid is-
+
+TARUNDEEP
+Give a number-6
+Give a number-4
+[6, 4, 10]
+Give y to contnue and n to stop going further-N
+Now we got a list having a fibonacci series-
+[6, 4, 10]
+Average of the sequence-
+6.666666666666667
+-------------(a)------------
+{1, 3, 5, 6, 8}
+-------------(b)------------
+{3, 6, 8, 9, 13, 17}
+-------------(c)------------
+{1, 2, 4, 5}
+-------------(d)------------
+{6, 7, 8, 9, 10}
+-------------(e)------------
+{10, 7}
+>>> 
